cfd9e4c1b40f4e98acbc05a063dd165c178ce9e5
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-07-29 16:26:31 +0300

    completed Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
+const [darkTheme, setDarkTheme] = useState(false)
+  function handleClick(){
+    setDarkTheme(!darkTheme);
+  }
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
-
+  const topNode = darkTheme ? "App dark" : "App light"
+  const darkMode = darkTheme? "Light Mode" : "Dark Mode"
   return (
-    <div className={appClass}>
+    <>
+    
+    <div className={topNode}>
       <header>
-        <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        {/* <h2>Shopster</h2>
+        <div className="App light">
+        <button onClick={handleClick}>
+          {darkMode}
+        </button>
+</div> */}
+{/* <div className="App dark">
+<button>Dark Mode</button>
+</div> */}
+<h2>Shopster</h2>
+<button onClick={handleClick}>{darkMode}</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
+    </>
   );
 }
 
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setaIsinCart] = useState(false)
+
+  function handleAddCart(){
+    setaIsinCart(!isInCart)
+  }
+
+  const itemClass = isInCart ? "in-cart" : ` ""` 
+  const itemText = isInCart ? "Remove from cart" : "Add to cart"
+  const buttonClass = isInCart ? "remove" : "add"
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={buttonClass} onClick={handleAddCart}>{itemText}</button>
     </li>
   );
 }
